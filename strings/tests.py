c1ae51e7539808a58f081c3255c76277f73e1d3d
Thu Than <than@uni.minerva.edu>
2025-08-07 20:02:51 +0700

    test: add more test cases

--- strings/tests.py
+++ strings/tests.py
@@ -27,6 +27,18 @@ class TestSplitString(unittest.TestCase):
         arr = split_string(s, c)
         self.assertEqual(arr, [])
 
+    def test_split_with_comma(self):
+        s = "123,456,789"
+        c = ","
+        arr = split_string(s, c)
+        self.assertEqual(arr, ["123", "456", "789"])
+
+    def test_split_with_special_char(self):
+        s = "!@#$%"
+        c = "@"
+        arr = split_string(s, c)
+        self.assertEqual(arr, ["!", "#$%"])
+
 
 if __name__ == "__main__":
     unittest.main()
